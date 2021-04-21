c1ca4f516e65246faa706612c16098ae9ccc50e7
Mikhail Gostishchev <>
2021-04-21 18:36:11 +0300

    complete context api vs redux lessons

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+
+import CollectionsContext from '../../contexts/collections/collections.context';
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+
+
+import './collections-overview.styles.scss';
+
+const CollectionsOverview = () => {
+  const collectionsMap = useContext(CollectionsContext);
+  const collections = Object.keys(collectionsMap).map(
+    key => collectionsMap[key]
+  );
+  return (
+    <div className='collections-overview'>
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+
+export default CollectionsOverview;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './App';
+import CartProvider from './providers/cart/cart.provider';
+
+ReactDOM.render(
+  <CartProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </CartProvider>,
+
+  document.getElementById('root')
+);
